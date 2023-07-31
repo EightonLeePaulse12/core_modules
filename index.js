9ef47baff3d0e4f0bae16a29d9f816cc68ad507d
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-07-31 12:37:57 +0200

    Hello

---
+++ index.js
@@ -0,0 +1,16 @@
+// FS Example
+/*const fs = require('fs')
+fs.writeFileSync("add.js","console.log(2 + 6)")
+*/
+
+// PATH Example
+/*const path = require('path')
+const filename = path.basename('/code.js')
+console.log(filename)
+*/
+
+// OS example
+/*const os = require('os')
+const hostname = os.hostname()
+console.log(hostname)
+*/
